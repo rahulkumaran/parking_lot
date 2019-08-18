87a8354866458f1e4d6b494ef86b8fa56bd4fe2b
rahulkumaran <rahuldravid313@gmail.com>
2019-08-18 19:54:30 +0530

    minor fixes to accomodate and get input from command line while running parking_lot UNIX script

--- main.py
+++ main.py
@@ -2,17 +2,16 @@ import sys
 from parking_lot.parking import Cars
 
 if(__name__=='__main__'):
-    car = Cars()
-    if (len(sys.argv)==2):
-        file = open(sys.argv[1],'r+')
-        for line in file:
-            car.action(line.split(" "))
+	car = Cars()
+	if (".txt" in sys.argv[1]):
+		sys.argv[1] = sys.argv[1].replace('\n',"")
+		file = open(sys.argv[1],'r+')
+		for line in file:
+			car.action(line.split(" "))
 
-    elif(len(sys.argv)==1):
-        command = ""
-        while(True):
-            command = input("\n$ ")
-            car.action(command.split(" "))
+	else:
+		command = ""
+		while(True):
+			command = input("\n$ ")
+			car.action(command.split(" "))
 
-    else:
-        print("Please run the program in the right manner!")
