19bf7a64b5b36799db0cbe4e2c34aaafd355889b
Rahul Arulkumaran <rahuldravid313@gmail.com>
2019-08-18 18:47:36 +0530

    update code wrt updated classes

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 import sys
-from parking_lot.parking import ParkingLot
+from parking_lot.parking import Cars
 
 if(__name__=='__main__'):
-    parking = ParkingLot()
+    car = Cars()
     if (len(sys.argv)==2):
         file = open(sys.argv[1],'r+')
         for line in file:
-            parking.action(line.split(" "))
+            car.action(line.split(" "))
 
     elif(len(sys.argv)==1):
         command = ""
         while(True):
             command = input("\n$ ")
-            parking.action(command.split(" "))
+            car.action(command.split(" "))
 
     else:
         print("Please run the program in the right manner!")
