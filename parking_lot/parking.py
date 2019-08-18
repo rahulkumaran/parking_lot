864ac3486dc33acb5e0cb5cc1d6bbd0a9ad3ec8f
Rahul Arulkumaran <rahuldravid313@gmail.com>
2019-08-18 17:22:02 +0530

    update code with exception handling and proper messages to help users understand if command was wrong and avoid abrupt closing of program

--- parking_lot/parking.py
+++ parking_lot/parking.py
@@ -2,7 +2,7 @@ import sys
 
 class ParkingLot:
     def __init__(self):
-        self.slots = 0
+        self.slots = -1
         self.blocked = []
         self.occupancy = []
 
@@ -14,12 +14,16 @@ class ParkingLot:
 
         slots : string
         '''
-        self.slots = int(slots)
-        if('\n' in str(slots)):
-            print("Created a parking lot with " + str(slots[0:-1]) + " slots")
-        else:
-            print("Created a parking lot with " + str(slots[0]) + " slots")
-        return slots
+        try:
+            self.slots = int(slots)
+            if('\n' in str(slots)):
+                print("Created a parking lot with " + str(slots[0:-1]) + " slots")
+            else:
+                print("Created a parking lot with " + str(slots[0]) + " slots")
+            return slots
+        except ValueError:
+            print("Please enter an integer number of slots!")
+            return 0
 
     def park(self, reg_no, colour):
         '''
@@ -29,17 +33,23 @@ class ParkingLot:
         reg_no : string
         colour : string
         '''
-        if(len(self.blocked)==self.slots):
-            print("Sorry, parking lot is full")
-        else:
-            all_slots = [x for x in range(1, self.slots + 1)]
-            block_list = set(self.blocked)
-            #print(all_slots)
-            available = list(block_list ^ set(all_slots))
-            self.blocked += [available[0]]
-            self.occupancy += [[available[0], reg_no, colour]]
-            print("Allocated slot number: " + str(available[0]))
-        return reg_no
+        try:
+            if(self.slots == -1):
+                print("Please create a parking lot first to park cars!")
+            elif(len(self.blocked)==self.slots):
+                print("Sorry, parking lot is full")
+            else:
+                all_slots = [x for x in range(1, self.slots + 1)]
+                block_list = set(self.blocked)
+                #print(all_slots)
+                available = list(block_list ^ set(all_slots))
+                self.blocked += [available[0]]
+                self.occupancy += [[available[0], reg_no, colour]]
+                print("Allocated slot number: " + str(available[0]))
+            return reg_no
+        except:
+            print("Sorry, faced an unexpected trouble. Please type in your command again!")
+            return 0
 
     def leave(self, slot):
         '''
@@ -49,18 +59,22 @@ class ParkingLot:
 
         slot : string
         '''
-        position = 0
-        for car in self.occupancy:
-            if(int(slot) in car):
-                del self.occupancy[position]
-                del self.blocked[position]
-                break
-            position += 1
-        if('\n' in str(slot)):
-            print("Slot number " + str(slot[0:-1]) + " is free")
-        else:
-            print("Slot number " + str(slot) + " is free")
-        return slot
+        try:
+            position = 0
+            for car in self.occupancy:
+                if(int(slot) in car):
+                    del self.occupancy[position]
+                    del self.blocked[position]
+                    break
+                position += 1
+            if('\n' in str(slot)):
+                print("Slot number " + str(slot[0:-1]) + " is free")
+            else:
+                print("Slot number " + str(slot) + " is free")
+            return slot
+        except ValueError:
+            print("Please enter an integer number of slots!")
+            return 0
 
     def status(self):
         '''
@@ -69,6 +83,8 @@ class ParkingLot:
         period.
 
         '''
+        if(len(self.blocked)==0):
+            print("No cars found in parking lot at the moment.")
         print("Slot No.\tRegistration No\t\tColour")
         cars = self.occupancy
         for details in cars:
@@ -83,13 +99,20 @@ class ParkingLot:
 
         colour : string
         '''
-        cars = self.occupancy
-        statement = ""
-        for details in cars:
-            if(colour in details):
-                statement += details[1] + ", "
-        print(statement[0:-2])
-        return statement
+        try:
+            cars = self.occupancy
+            statement = ""
+            for details in cars:
+                if(colour in details):
+                    statement += details[1] + ", "
+            if(statement == ""):
+                print("No cars found in the parking lot with the colour " + colour)
+            else:
+                print(statement[0:-2])
+            return statement
+        except:
+            print("Sorry, faced an unexpected trouble. Please type in your command again!")
+            return 0
 
     def slot_numbers_for_cars_with_colour(self, colour):
         '''
@@ -104,7 +127,10 @@ class ParkingLot:
         for details in cars:
             if(colour in details):
                 statement += str(details[0]) + ", "
-        print(statement[0:-2])
+        if(statement == ""):
+            print("No cars found in the parking lot with the colour " + colour)
+        else:
+            print(statement[0:-2])
         return statement
 
     def slot_number_for_registration_number(self, reg_no):
